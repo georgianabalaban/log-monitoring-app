03f160dca3dbd1c7426428d354583391def80694
Georgiana Balaban <georbalaban@gmail.com>
2025-04-09 19:42:00 +0300

    feat: add utility constants

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { __dirname } from "./utils/pathUtils.js";
+import path from "path";
+
+const logFilePath = path.join(__dirname, "logs.log");
+const outputFilePath = path.join(__dirname, "log_report.txt");
---
+++ src/utils/threshholds.js
@@ -0,0 +1,2 @@
+export const WARNING_THRESHOLD = 5 * 60; // 5 minutes in seconds
+export const ERROR_THRESHOLD = 10 * 60; // 10 minutes in seconds
